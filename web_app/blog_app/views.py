838e481b6efa7cd2a00ebb2bc6c51470236b9897
Collins Kechir <kechircollins@gmail.com>
2024-03-29 22:27:33 +0300

    Update view and url files for post delete CBV

--- web_app/blog_app/views.py
+++ web_app/blog_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 from .models import Post
 
@@ -66,6 +66,23 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         if self.request.user == post.author:
             return True
         return False
+
+
+# Delete view
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+
+    # Test user 
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+
+
 
 def about(request):
     return render(request, 'blog_app/about.html', {'title':'About Page'})
